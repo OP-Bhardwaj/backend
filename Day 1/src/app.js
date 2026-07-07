ffcade7e4973d86b03a732e723a007b31b6e05e4
OP Bhardwaj <234922590+OP-Bhardwaj@users.noreply.github.com>
2026-07-07 11:37:35 +0530

    first commit

---
+++ Day 1/src/app.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const noteModel = require("src/models/note.model")
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+app.post('/notes', async (req, res) => {
+  const data = req.body
+  await noteModel.create({
+    title: data.title,
+    description: data.description
+  })
+
+  res.status(200).json({
+    message: "note created successfully"
+  })
+
+})
+
+//  GET METHOD to get data from server----------
+app.get('/notes', (req, res) => {
+
+  res.status(200).json({
+    message: "notes fetched successfully",
+    notes: notes
+  })
+})
+
+// DELETE METHOD to delete anything----------------------------
+
+app.delete('/notes/:index', (req, res) => {
+
+  const index = req.params.index
+
+  delete notes[index]
+
+  res.status(200).json({
+    message: "note deleted successfully"
+  })
+})
+
+// PATCH METHOD to update anything -----------------------------------------
+
+app.patch('/notes/:index', (req, res) => {
+
+  const index = req.params.index
+  const description = req.body.description
+  const title = req.body.title
+
+  notes[index].description = description
+  notes[index].title = title
+
+  res.status(200).json({
+    message: "notes updated successfully"
+  })
+})
+ 
+
+
+
+module.exports = app;
+
+
+echo "# backend" >> README.md
