7a13665b9f807a17fcfa8d98eca3034788d95a54
OP Bhardwaj <234922590+OP-Bhardwaj@users.noreply.github.com>
2026-08-03 14:32:41 +0530

    Refactor server.js to log MONGO_URI and improve code structure

--- day-2/Backend/server.js
+++ day-2/Backend/server.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 
-require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/config/database');
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
 
 connectDB();
 
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});  
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port", process.env.PORT || 3000);
+});
