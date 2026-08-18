c5b6016689a6d19bfcbce616091a5e25e39e667d
OP Bhardwaj <234922590+OP-Bhardwaj@users.noreply.github.com>
2026-08-18 10:29:56 +0530

    Refactor code structure for improved readability and maintainability

---
+++ day-4/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+
+
+
+const app = express()
+
+app.use(express.json());
+// const router = require("./routes/routes");
+// app.use(router);
+
+
+
+
+module.exports = app;
